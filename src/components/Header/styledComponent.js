e97401e8f4b79bf178f272fbfefa57f7fddc1f27
chandana <chanduvadla499@gmail.com>
2025-11-10 10:42:32 +0000

    updated

---
+++ src/components/Header/styledComponent.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+  align-items: center;
+`
+
+export const CafeName = styled.h1`
+  font-size: 24px;
+`
